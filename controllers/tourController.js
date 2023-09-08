99139be3e81e33d4ab49ec6ec92df25069921e83
Armen Dela Cruz <delacruzrmn@gmail.com>
2023-09-08 15:52:41 +0800

    Added param and body middlewares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,28 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkId = (req, res, next, val) => {
+  if (val * 1 > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Could not find tour.',
+    });
+
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  const { name, price } = req.body;
+
+  if (!name || !price)
+    return res.status(400).json({
+      status: 'error',
+      message: 'Name and/or price is missing.',
+    });
+
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -15,11 +37,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((t) => t.id === id);
-  if (!tour)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Could not find tour.',
-    });
 
   res.status(200).json({
     status: 'success',
@@ -29,12 +46,11 @@ exports.getTour = (req, res) => {
 };
 
 exports.createTour = (req, res) => {
-  console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
@@ -46,12 +62,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
   res.status(200).json({
     status: 'success',
     data: {
@@ -61,12 +71,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-
   res.status(204).json({
     status: 'success',
     data: null,
